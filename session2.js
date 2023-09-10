c1448548c9c3305cad0d9f1b170a1fa8dd3c19ab
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-10 14:36:40 +0330

    first commit

---
+++ session2.js
@@ -0,0 +1,18 @@
+let age = 18
+let sum = 18*3
+
+console.log(age)
+console.log(sum)
+
+console.log("Hi, my name is Melina!")
+
+
+let counter = 0
+counter += 10
+console.log(counter)
+
+counter ++ 
+console.log(counter)
+
+console.log( 5 != 5)
+console.log(10 < 22)
